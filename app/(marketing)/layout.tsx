4bf14eb393f9cd1d1ea777309667b7d186a32bfe
summer030 <karaage4projects@outlook.com>
2023-10-25 23:32:27 +0800

    feat: landing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "./_components/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full dark:bg-[#1F1F1F]">
+      <NavBar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
