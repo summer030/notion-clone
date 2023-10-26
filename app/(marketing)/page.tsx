985ff58caae98b33e9af03d27531de92f3446215
summer030 <125527872+summer030@users.noreply.github.com>
2023-10-26 20:51:21 +0800

    fix: errors

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
 import { Footer } from "./_components/footer";
