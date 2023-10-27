c63c632cfb4db8279e6f56024f223ab1a9fb293b
summer030 <125527872+summer030@users.noreply.github.com>
2023-10-27 23:04:26 +0800

    feat: sidebar

---
+++ app/(main)/(routes)/_components/navigation.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ElementRef, useRef, useState } from "react";
+import { ChevronsLeft } from "lucide-react";
+
+export const Navigation = () => {
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"aside">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <>
+      <aside className="group/sidebar h-full w-60 bg-secondary overflow-y-auto relative flex flex-col z-[99999]">
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action Items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  );
+};
